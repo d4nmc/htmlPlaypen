fbae8e5724d03d27be184cbd72a2f774fc7a4ee9
d4nmc <danielmcgee.work@gmail.com>
2021-07-14 17:42:00 +0100

    reviewing the calc exercise and breaking it down

---
+++ calcExercise/calc.js
@@ -0,0 +1,28 @@
+`use strict`
+
+// <button type="submit" id="btn" name="submit">Submit</button>
+
+//* <h1>Calculator</h1>
+
+//<form action="" method="POST">
+//   <input type="number1" id="num1" placeholder="Enter Number" /> <br><br>
+//   <input type="number2" id="num2" placeholder="Enter Number" />
+//   </form> */}
+
+// creating calculator functions
+// Document.querySelector("#num1");
+// Document.querySelector("#num2");
+
+// get the div with the heading in from the html doc to use as a base example
+//let divAddToMe = document.querySelector("#heading");
+
+//divAddToMe.appendChild(newHeading);
+
+addition(3,4);
+
+// 1. CRUD stuff
+
+const addition = (a,b) => console.log(a+b);
+const subtraction = (a,b) => console.log(a-b);
+const multiply = (a,b) => console.log(a*b);
+const divide = (a,b) => console.log(a/b);
